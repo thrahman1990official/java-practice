e844bb38d28bb7a6d070de88919f9aa9a8bef5a9
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2020-11-25 23:34:51 -0500

    Create keywords_and_expressions.java

---
+++ keywords_and_expressions.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+	//mile is equal to 1.609344 kilometers
+        double kilometres = (100 * 1.609344);
+        int highScore = 50;
+
+        if(highScore == 50)
+        {
+          System.out.println("This is an expression");
+        }
+
+        int score = 100;
+        if (score >99)
+        {
+            System.out.println("You got high score!");
+            score = 0;
+        }
+    }
+}
