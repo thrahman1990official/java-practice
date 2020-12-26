926d79649644f48888e9430f14bf94b2a33f7298
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2020-12-26 03:11:46 -0500

    Update javaMethodsExample2.java

--- javaMethodsExample2.java
+++ javaMethodsExample2.java
@@ -33,16 +33,25 @@ public class javaMethodsExample2
         //
 
             int highScorePosition = calculateHighScorePosition(1500);
-                displayHighScorePosition("Tim", highScorePosition);
+                displayHighScorePosition("Ali", highScorePosition);
 
                 highScorePosition = calculateHighScorePosition(900);
-                displayHighScorePosition("Bob", highScorePosition);
+                displayHighScorePosition("Hamzah", highScorePosition);
 
                 highScorePosition = calculateHighScorePosition(400);
-                displayHighScorePosition("Percy", highScorePosition);
+                displayHighScorePosition("Zubair", highScorePosition);
 
                 highScorePosition = calculateHighScorePosition(50);
-                displayHighScorePosition("Gilbert", highScorePosition);
+                displayHighScorePosition("Talhah", highScorePosition);
+
+                highScorePosition = calculateHighScorePosition(1000);
+                displayHighScorePosition("Othman", highScorePosition);
+
+                highScorePosition = calculateHighScorePosition(500);
+                displayHighScorePosition("Omar", highScorePosition);
+
+                highScorePosition = calculateHighScorePosition(100);
+                displayHighScorePosition("Bilal", highScorePosition);
     }
 
     public static void displayHighScorePosition(String playerName, int highScorePosition)
@@ -52,15 +61,15 @@ public class javaMethodsExample2
 
     public static int calculateHighScorePosition(int playerScore)
     {
-        if(playerScore > 1000)
+        if(playerScore >= 1000)
         {
             return 1;
         }
-        else if(playerScore < 500 && playerScore < 1000)
+        else if(playerScore >= 500 && playerScore < 1000)
         {
             return 2;
         }
-        else if(playerScore < 100 && playerScore < 500)
+        else if(playerScore >= 100 && playerScore < 500)
         {
             return 3;
         }
@@ -71,7 +80,8 @@ public class javaMethodsExample2
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
-        if (gameOver) {
+        if (gameOver)
+        {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
             return finalScore;
