35ca079ea20100ca960c01c203865b407250ffdd
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-07-31 00:51:45 -0700

    Create ES6-Problem1.js

---
+++ ES6-Problem1.js
@@ -0,0 +1,4 @@
+//Write JavaScript program to compare two objects to determine if first one contains equivalent property values to second one
+//Use Object.keys() to get all keys of second object
+//Use Array.prototype.every(), Object.prototype.hasOwnProperty() and strict comparison to determine if all keys exist in first object and have same values
+
