b257c0e73536c01c24241490183806a7c773c51f
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-08-05 21:35:57 -0700

    Create JavaSession1Problem1.java

---
+++ JavaSession1Problem1.java
@@ -0,0 +1,7 @@
+//Stdin & Stdout problem
+//You need read first integer from stdin, which will tell you # of elements you need to read
+//After that you need to read all those numbers & print them all
+//example:
+//Input: 3, 12, 23, 45
+//Output: 12, 23, 45
+
