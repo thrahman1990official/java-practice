cb1abe1c1c4e1fe70316e133ddef3977890e825d
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-08-05 21:58:00 -0700

    Update JavaSession1Problem1.java

--- JavaSession1Problem1.java
+++ JavaSession1Problem1.java
@@ -5,3 +5,18 @@
 //Input: 3, 12, 23, 45
 //Output: 12, 23, 45
 
+import java.util.Scanner; //importing java library for scanning
+
+public class StdInStdOut    //Creating class called StdInStdOut
+{
+  public static void main(String[] args)    //Creating main class
+  {
+   Scanner scanner = new Scanner(System.in);    //Creating scanner class input
+    
+    int count = scanner.nextInt();    //Creating a count variable that scans next couple of integers
+    
+    int myIntArray[] = new int[count];
+    int index = 0;
+    
+  }
+}
