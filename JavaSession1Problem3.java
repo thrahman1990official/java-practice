069231001081c6710eefb1a386e5c4f588fda40d
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-08-06 21:47:44 -0700

    Create JavaSession1Problem3.java

---
+++ JavaSession1Problem3.java
@@ -0,0 +1,11 @@
+//In this problem, you need to first read integers x, y, & n, from stdin.
+//Using these numbers, print following series:
+//(x + ((3^0)*y)), (x + ((3^0)*y) + ((3^1)*y)) ... (x + ((3^0)*y) + ((3^1)*y) + (3^(n-1)*y)
+//input
+//0
+//2
+//3
+//output
+//2
+//8
+//26
