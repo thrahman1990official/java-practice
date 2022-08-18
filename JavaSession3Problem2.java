4bd5e2e49e9005e083ded9012cee13427fa231bf
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-08-17 21:54:57 -0700

    Create JavaSession3Problem2.java

---
+++ JavaSession3Problem2.java
@@ -0,0 +1,23 @@
+//Given input string, remove all duplicate words from string.
+
+//for given input:
+//Hello world hello world world
+
+//the resulting output should be:
+//Hello world
+
+//Approach taken: Split input string into words
+//Use LinkedHashSet to remove duplicates
+
+import.java.util.Locale;
+
+public class RemoveDuplicates
+  {
+  public static void main(String[] args)
+    {
+    String input = "Hello world hello world world";
+    
+  input = input.toLowerCase(locale.ROOT);
+    
+    }
+  }
