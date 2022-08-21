29183481103ce659b772ca1819bbd66392102b88
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-08-20 21:50:53 -0700

    Create JavaSession3Problem4.java

---
+++ JavaSession3Problem4.java
@@ -0,0 +1,26 @@
+//Given an array of student objects, which contains student name & score
+//Write function sort them by their scores
+//If scores are same & sort them by their names
+
+//Input
+//  Andrew 100
+//  Jack 75
+//  Justin 75
+//  Joe 125
+//  Amy 100
+
+//Output
+//  Joe 125
+//  Amy 100
+//  Andrew 100
+//  Jack 75
+//  Justin 75
+
+//  Approach
+//  CompareTo() method
+//  if s1 > s2, it returns positive number
+//  if s1 < s2, it returns negative number
+//  if s1 == s2, it returns zero
+
+
+
