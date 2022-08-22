17449f6634baf23bd25b8caa15d4fc1e15ca7086
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-08-21 20:54:50 -0700

    Create JavaStdinStdout.java

---
+++ JavaStdinStdout.java
@@ -0,0 +1,16 @@
+//NOTE: ALWAYS PRINT STATEMENTS IN THE MAIN METHOD
+
+public class JavaStdinStdout
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Talha Rahman");
+    
+    myfunction(); //calls the static function below
+  }
+  
+  static void myfunction()
+      {
+      System.out.println("Talha Rahman is the tutor");
+      }
+}
