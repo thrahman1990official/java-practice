bdb6528b44439115172d1a515ba91059895e47b2
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-09-13 18:24:22 -0700

    Create JavaSessionOutputFormatting.java

---
+++ JavaSessionOutputFormatting.java
@@ -0,0 +1,9 @@
+//Input format
+//Every line of input will contain string followed by an Integer
+//Each string will have a maximum of 10 Alphatics characters, and each integer will be in inclusive range from 0 to 999
+
+//Output format
+//In each line of output there should be two columns:
+//The first column contains String & is left justified using exactly 15 characters
+//The second column contacts Integer, expressed in exactly in 3 digits, if original input has less than three digits, you must pad your output's
+//leading digits with zeroes
