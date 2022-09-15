02a270be832f1b040693a48aa7a6bf791ccb0d3a
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-09-14 19:22:13 -0700

    Create JavaSessionLoops2.java

---
+++ JavaSessionLoops2.java
@@ -0,0 +1,29 @@
+//We use integers a, b, & n, to create following series:
+
+//(a + (2^0) * b), (a + ((2^0) * b) + ((2^1) * b)),...,(a + ((2^0) * b) + ((2^1) * b))
+
+//You are given 'q' queries in form of a, b, & n. For each query, print series corresponding to given a, b, & n values, as a single line of 'n' space
+//separated integers.
+
+//INPUT FORMAT
+//first line contains an integer 'q' denoting number of queries
+//Each line 'i' of 'q' subsequent lines contains three space-separated integers
+//describing respective a(i), b(i), & n(i) values for that query
+
+//CONSTRAINTS
+//0<=q<=500
+//0<=q<=50
+//1<=n<=15
+
+//OUTPUT FORMAT
+//For each query, print corresponding series on new line. Each series must be printed in order as single line of 'n' space-separated integers
+
+import java.util.Scanner;
+
+public class JavaSessionLoops2
+  {
+  public static void main(String[] args)
+    {
+    Scanner scanner = new Scanner(System.in);
+    }
+  }
