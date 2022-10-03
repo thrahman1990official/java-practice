eb36c0d2774570899dce7d31688c685c7dfe1a11
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-10-03 02:42:04 -0700

    Update java_application_development_interview_question1.java

--- java_application_development_interview_question1.java
+++ java_application_development_interview_question1.java
@@ -15,6 +15,16 @@ public class java_application_development_interview_question1
         }
       } 
     }
+    private boolean flag = true;
+    
+    public void testing()
+      {
+      System.out.println("Testing");
+      }
+    public abc()
+      {
+      (new Inner()).sample();
+      }
   }
   //Examine following code, it includes inner class, what will be output of the following code
   public class Main
@@ -26,3 +36,7 @@ public class java_application_development_interview_question1
     }
 //till the above bracket if you are testing on an online compiler
 }
+
+//Concepts to know: Constructor, Final Class
+//Constructor is special function called only at the time of objectation & name is same as class name
+
