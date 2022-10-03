56219c51e1f81ee0992c5c0b2fc04f0baeb77580
thrahman1990official <52221686+thrahman1990official@users.noreply.github.com>
2022-10-03 02:34:11 -0700

    Create java_application_development_interview_question1.java

---
+++ java_application_development_interview_question1.java
@@ -0,0 +1,28 @@
+import java.util.*
+
+public class java_application_development_interview_question1
+{
+// You can run the code starting from public ... class abc
+  public final class abc
+  {
+   class Inner
+    {
+    void sample()
+      {
+      if(abc.this.flag)
+        {
+        testing();
+        }
+      } 
+    }
+  }
+  //Examine following code, it includes inner class, what will be output of the following code
+  public class Main
+    {
+    public static void main(String [] args)
+      {
+      new abc();
+      }
+    }
+//till the above bracket if you are testing on an online compiler
+}
